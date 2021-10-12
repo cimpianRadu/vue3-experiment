cb5a3af266b54968e4147ef95e49e0b60c96a53f
RaduGCimpian <cimpian.radu@yahoo.com>
2021-10-12 21:32:53 +0300

    feature: setup the router

---
+++ WiskeyHunterApp/src/views/home.vue
@@ -0,0 +1,33 @@
+<script setup>
+import CategoryCard from '../components/CategoryCard.vue';
+
+const categories = [
+  {
+    name: 'Distiliries',
+    imageSource: 'https://source.unsplash.com/QL3SaEwio_k',
+  },
+  {
+    name: 'Wiskeys',
+    imageSource: 'https://source.unsplash.com/55bQ7g3ZlQU',
+  },
+];
+</script>
+
+<template>
+  <div id="container">
+    <CategoryCard
+      v-for="(category, index) in categories"
+      :category="category.name"
+      :imgSource="category.imageSource"
+      :key="index"
+    />
+  </div>
+</template>
+
+<style scoped>
+#container {
+  display: flex;
+  justify-content: space-around;
+  height: 100%;
+}
+</style>
