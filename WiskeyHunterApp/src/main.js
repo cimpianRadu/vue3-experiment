6b75dad032462504cd96d1298a14f582d4d7b62b
RaduGCimpian <cimpian.radu@yahoo.com>
2021-10-15 15:54:21 +0300

    chore: add balm ui

--- WiskeyHunterApp/src/main.js
+++ WiskeyHunterApp/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import BalmUI from 'balm-ui'; // Official Google Material Components
+import BalmUIPlus from 'balm-ui-plus'; // BalmJS Team Material Components
+import 'balm-ui-css';
 import router from '../src/router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(BalmUI).use(BalmUIPlus).mount('#app');
