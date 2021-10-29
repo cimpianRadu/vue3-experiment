652a0d3b22523c07a3d01008f654c42385ff5908
RaduGCimpian <cimpian.radu@yahoo.com>
2021-10-29 16:30:09 +0300

    feat: display cocktails

--- WiskeyHunterApp/src/components/Cocktail.vue
+++ WiskeyHunterApp/src/components/Cocktail.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { COLORS } from '../constants';
+
 const props = defineProps({
   drink: {
     idDrink: Number,
@@ -6,12 +8,42 @@ const props = defineProps({
     strDrinkThumb: String,
   },
 });
-
-console.log(props.drink);
 </script>
 
 <template>
   <div>
-    <p>Cocktail {{ drink.strDrink }}</p>
+    <ui-card id="container">
+      <ui-card-content>
+        <ui-card-media
+          id="content"
+          rectangle
+          v-bind:style="{ backgroundImage: 'url(' + drink.strDrinkThumb + ')' }"
+        >
+        </ui-card-media>
+      </ui-card-content>
+      <ui-card-actions>
+        <ui-card-icons>
+          <p id="text">{{ drink.strDrink }}</p>
+        </ui-card-icons>
+      </ui-card-actions>
+    </ui-card>
   </div>
 </template>
+
+<style scoped>
+#container {
+  height: 250px;
+  width: 400px;
+  margin-block: 15px;
+}
+
+#content {
+  object-fit: cover;
+  background-color: coral;
+}
+
+#text {
+  font-size: 18px;
+  font-weight: 700;
+}
+</style>
