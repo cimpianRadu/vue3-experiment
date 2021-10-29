da8c9136f640e209db0613af02c306f9a41e87b4
RaduGCimpian <cimpian.radu@yahoo.com>
2021-10-29 12:46:49 +0300

    feat: get drinks category

--- WiskeyHunterApp/src/components/Categories.vue
+++ WiskeyHunterApp/src/components/Categories.vue
@@ -1,18 +1,30 @@
 <script setup>
+import { ref } from 'vue';
 import { getCategories } from '../api/get-categories';
+import CocktailCategory from '../components/CocktailCategory.vue';
 
 const categories = await getCategories({ requestUrl: 'list.php?c=list' });
+const active = ref(0);
 </script>
 
 <template>
   <div id="container">
-    <ui-tab-bar v-model="active" class="hero-demo-tab-bar">
-      <ui-tab v-for="(item, index) in categories.drinks" :key="index">{{
-        item.strCategory
-      }}</ui-tab>
+    <ui-tab-bar v-model="active">
+      <ui-tab v-for="(item, index) in categories.drinks" :key="index">
+        {{ item.strCategory }}
+      </ui-tab>
     </ui-tab-bar>
   </div>
+  <Suspense>
+    <template #default>
+      <CocktailCategory :category="categories.drinks[active].strCategory" />
+    </template>
+    <template #fallback>
+      <h3>Loading cocktail...</h3>
+    </template>
+  </Suspense>
 </template>
+
 <style scoped>
 #container {
   flex: 1;
